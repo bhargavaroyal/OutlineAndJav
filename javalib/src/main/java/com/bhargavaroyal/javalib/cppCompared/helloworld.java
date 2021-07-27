0221b8e65329a5556a342ff0de375d8f2e2269c2
bhargavaroyal <bhargavaroyal@gmail.com>
2021-07-27 20:35:20 +0530

    Create helloworld.java

---
+++ javalib/src/main/java/com/bhargavaroyal/javalib/cppCompared/helloworld.java
@@ -0,0 +1,7 @@
+package com.bhargavaroyal.javalib.cppCompared;
+
+public class helloworld {
+/*    public static void main(String[] args) {
+        System.out.println("Hello world from Java");
+    }*/
+}
